919890ee80cc1a6aae3d2ed367473811d91a4b32
Jannes-R <s_radtkej22@hwr-berlin.de>
2025-02-21 22:15:25 +0100

    add scraper for supermarkets; add scraper to index.py

---
+++ app/webscraping/scraperSupermarkt.py
@@ -0,0 +1,38 @@
+import re
+import string
+from urllib.request import urlopen
+
+
+def run():
+    supermarkets = []
+    BASE_URL = "https://berlin.kauperts.de/Adressen/Supermarkt/{letter}"
+    # Durch alle Buchstaben A-Z iterieren
+    for letter in string.ascii_uppercase:  # ['A', 'B', ..., 'Z']
+        url = BASE_URL.format(letter=letter)
+        print(f"Rufe Daten von {url} ab...")
+        page = urlopen(url)
+        html_bytes = page.read()
+        html = html_bytes.decode("utf-8")
+
+        # Filtern aller Markt-Namen auf der Seite
+        marketNamePattern = r"<td class=\"updated\"><a href=\".*\">(.*)<\/a><\/td>"
+        marketNames = re.findall(marketNamePattern, html)
+        # Filtern aller Adressen auf der Seite
+        locationPattern = r"<td class=\"secondary zip-city\">\n(.*\n.*\n.*)<\/td>"
+        locations = re.findall(locationPattern, html)
+        for name, adress in zip(marketNames, locations):
+            # Adressen 'schön machen'
+            adress = adress.replace("&ndash;\n", " ").replace("&nbsp;", " ").replace("\n      ", "").replace("        ", "")
+            # Postleitzahl aus Adresse herausfiltern
+            plzPattern = r"\d\d\d\d\d"
+            plz = re.findall(plzPattern, adress)
+            # Alle Daten in supermarkets gespeichert
+            supermarkets.append({"plz": plz,"name": name.replace("&nbsp;", ""), "adresse": adress})
+
+
+    return supermarkets
+    # Hier später einfügen in DB
+
+
+
+print(run())
