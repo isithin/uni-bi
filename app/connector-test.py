64dd08ddaf5efc68cf4bd997b088257321f601d6
isithin <xisithinx@gmail.com>
2025-02-22 21:43:03 +0000

    prettier python code

--- app/connector-test.py
+++ app/connector-test.py
@@ -6,24 +6,52 @@ import os
 time.sleep(10)
 
 # Verbindung zu MySQL herstellen
-db = mysql.connector.connect(
-    host=os.getenv("MYSQL_HOST"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    database=os.getenv("MYSQL_DATABASE")
-)
+def connect():
+    try:
+        db = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE")
+        )
+        return db
+    except Exception as e:
+        print(e)
+        return None
 
-cursor = db.cursor()
+def create_table(cursor):
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS example (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255)
+        )
+        """
+    )
+ 
+def insert_data(cursor, db):
+    cursor.execute("INSERT INTO example (name) VALUES ('MiiiTest')")
+    db.commit()
 
-# Beispiel: Tabelle erstellen & Daten einfügen
-cursor.execute("CREATE TABLE IF NOT EXISTS example (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))")
-cursor.execute("INSERT INTO example (name) VALUES ('MiiiTest')")
-db.commit()
+def read_data(cursor):
+    cursor.execute("SELECT * FROM example")
+    for row in cursor.fetchall():
+        print(row)
 
-# Daten abrufen
-cursor.execute("SELECT * FROM example")
-for row in cursor.fetchall():
-    print(row)
 
-cursor.close()
-db.close()
+def main():
+    db = connect()
+    if db is not None:
+        print("Connected to MySQL")
+    else:
+        print("Connection to MySQL failed")
+    cursor = db.cursor()
+
+    create_table(cursor)
+    insert_data(cursor, db)
+    read_data(cursor) # Just for debugging
+
+    cursor.close()
+    db.close()
+
+main()
