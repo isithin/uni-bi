77311e9e55f94f1f6a0fbe0d1b0174aac9c2104e
Jannes-R <s_radtkej22@hwr-berlin.de>
2025-02-24 14:09:52 +0100

    add boolean for discounter?

--- app/webscraping/scraperSupermarkt.py
+++ app/webscraping/scraperSupermarkt.py
@@ -5,6 +5,10 @@ from urllib.request import urlopen
 
 def run():
     supermarkets = []
+    # Liste mit typischen Discountern (kann erweitert werden)
+    discounter_keywords = [
+    "Aldi", "Lidl", "Penny", "Netto", "Norma"
+    ]
     BASE_URL = "https://berlin.kauperts.de/Adressen/Supermarkt/{letter}"
     # Durch alle Buchstaben A-Z iterieren
     for letter in string.ascii_uppercase:  # ['A', 'B', ..., 'Z']
@@ -24,11 +28,13 @@ def run():
             # Adressen und Namen 'schön machen'
             name = name.replace("&nbsp;", "")
             adress = adress.replace("&ndash;\n", " ").replace("&nbsp;", " ").replace("\n      ", "").replace("        ", "")
+            # discounter? ermitteln
+            discounter = any(discounter in name for discounter in discounter_keywords)
             # PLZ mit Regex extrahieren (5-stellige Zahl am Anfang der Adresse)
             plz_match = re.search(r"\b\d{5}\b", adress)
             plz = plz_match.group(0) if plz_match else "Unbekannt"
             # Alle Daten in supermarkets gespeichert
-            supermarkets.append({"plz": plz,"name": name, "adresse": adress})
+            supermarkets.append({"plz": plz,"name": name, "adresse": adress, "discounter?": discounter})
 
 
     return supermarkets
