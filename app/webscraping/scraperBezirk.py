5b16ec0702413f5bf786c64ae70fd45caa4ff20e
Jannes-R <s_radtkej22@stud.hwr-berlin.de>
2025-03-05 09:43:09 +0000

    delete one comma

--- app/webscraping/scraperBezirk.py
+++ app/webscraping/scraperBezirk.py
@@ -32,7 +32,7 @@ def scrape(db, cursor):
 
         if bezirk_match:
             bezirk = html.unescape(bezirk_match.group(1).strip())
-            data = "'"+bezirk+"',"
+            data = "'"+bezirk+"'"
             cursor.execute("INSERT IGNORE INTO Bezirk (Name) VALUES ("+data+")")
             db.commit()
 
