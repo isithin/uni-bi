a8d7c8b7725940cd83dec8989a5749e21e64353d
Jannes-R <s_radtkej22@stud.hwr-berlin.de>
2025-03-24 16:50:53 +0000

    bugfix

--- app/db_init.py
+++ app/db_init.py
@@ -68,7 +68,7 @@ def create_table(cursor):
         """
         CREATE TABLE IF NOT EXISTS Bezirk (
             Name                        VARCHAR(50) NOT NULL,
-            Fläche_Quadratkilometer     DECIMAL(3,2),
+            Fläche_Quadratkilometer     DECIMAL(5,2),
             Einwohner                   INT,
             Auslaender                  INT,
             Durchschnittsalter          DECIMAL(4,1), 
